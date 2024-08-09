78fc8b9b9f72212e0836396500c67331c4dc9a7c
MafanedzaFanie <142061001+MafanedzaFanie@users.noreply.github.com>
2024-08-09 17:17:02 +0200

    Project 2 Added Files

---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectTwo_35760338.Models;
+
+public partial class Client
+{
+    public Guid ClientId { get; set; }
+
+    public string? ClientName { get; set; }
+
+    public string? PrimaryContactEmail { get; set; }
+
+    public DateTime? DateOnboarded { get; set; }
+}
